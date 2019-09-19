db9dfc008a6dcefc82d2b48379e84682fd5e9b69
SimranVirk <simran.v.141@gmail.com>
2019-09-19 10:44:22 -0500

    adding mock event data for map

--- leaflet-example.js
+++ leaflet-example.js
@@ -8,7 +8,19 @@ Vue.component('event-pin', {
 	template: '<l-marker :lat-lng="loc"><l-popup>{{info}}</l-popup></l-marker>'}
 );
 
-// Load events from DB
+// Load events from DB - the data str
+// adding event enter name -- should it be a form with details or should they just enter it immediately
+/*
+using address or clicking or st else to create event
+what fields do we want when creating -- loc, date + time, about. (who else is going)
+*/
+
+//on startup -- request all the data from backend -- component mounting lifecycle --(for run on startup)
+
+//mock get data function - function to request something from backend - how do and where put
+//data points in east aldine
+// set default location and zoom
+
 
 // App instance
 new Vue({
@@ -20,26 +32,50 @@ new Vue({
 		url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
 		attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 		events: [
-			{info: "event 1", loc: L.latLng(47.413820, -1.219582)},
-			{info: "event 2", loc: L.latLng(47.414120, -1.219382)},
-			{info: "event 3", loc: L.latLng(47.413220, -1.219482)}
-		]
+			// {info: "event simran", loc: L.latLng(47.413820, -1.219582)},
+			// {info: "event 2", loc: L.latLng(47.414120, -1.219382)},
+			// {info: "event 3", loc: L.latLng(47.413220, -1.219482)}
+		],
+		newEventname: "New event"
 	},
+	mounted: function() {this.fetch_data()},
 	methods: {
 		listenEventLoc(e) {
 			console.log("wix test");
-			if (confirm("add event? click on desired location")) {
-				this.$refs.map.mapObject.on("click", this.trackEventLoc);
+			var newEventname = prompt("Please enter event name", "New event")
+			if (newEventname != null) {
+				this.newEventname = newEventname
+
+				if (confirm("add event? click on desired location")) {
+					this.$refs.map.mapObject.on("click", this.trackEventLoc);
+				}	
 			}
+			
 		},
 		trackEventLoc(e) {
 			console.log(e)
-			if (confirm("creat event at this location? "+e.latlng.lat+", "+e.latlng.lng)) {
-				this.events.push({info: "new event", loc: e.latlng});							
+			if (confirm("create event at this location? "+e.latlng.lat+", "+e.latlng.lng)) {
+				//check that event isnt a duplicate
+				this.events.push({info: this.newEventname, loc: e.latlng});							
 			}
 			this.$refs.map.mapObject.off("click");
+			this.newEventname = "New Event"
+
+			//add to database of events
+		},
+		fetch_data() {
+			//load mock data
+			var url = "https://gist.githubusercontent.com/stevenkuipers/1c0284ce4bb8a25a634613c37b60a333/raw/5de5c6d77ef3879474bd7e9a0dce70d032c2ca17/placeholder.json"
+			fetch(url).then(function(response) {
+				response.json().then(function(json) {
+					console.log("recevied json", json)
+
+				});
+
+			}, response => console.log("error loading json", response));
 		}
 	}
 });
+
 
 
